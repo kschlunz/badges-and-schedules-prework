c7053d2d53b72bb22e00cec311b7d28a094f22df
Kate Schlunz <schlunzk@gmail.com>
2018-02-28 16:50:48 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,8 +7,17 @@ def batch_badge_creator(badge_maker)
   badges = []
   badge_maker.each do |attendees|
     badges.push("Hello, my name is #{attendees}.")
-
-
   end
   badges
+end
+
+
+def assign_rooms(attendees)
+  
+  room_assignments =[]
+  attendees.each do |attendee|
+    room = 1
+    room_assignments.push("Hello, #{attendees}! You'll be assigned to room #{room}!")
+    room =+1
+  end
 end
