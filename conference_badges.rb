beef8d293318af3e1544ed75080a01bc9d776214
Kate Schlunz <schlunzk@gmail.com>
2018-02-28 16:34:39 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,8 +3,9 @@ def badge_maker(name)
 end
 
 
-def batch_badge_creator(names)
-  names.each do |badge_maker|
+def batch_badge_creator(attendees)
+  attendees.each do |name|
+    badge_maker
 
   end
 end
