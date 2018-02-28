bb7c5247fb864b070d3d15482e7b1eed3c434ba8
Kate Schlunz <schlunzk@gmail.com>
2018-02-28 16:57:30 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,11 +15,12 @@ end
 def assign_rooms(batch_badge_creator)
 
   room_assignments =[]
+  room = 0 
   batch_badge_creator.each do |attendees|
 
+    room+=1
 
-
-    room_assignments.push("Hello, #{attendees}! You'll be assigned to room #{1+}!")
+    room_assignments.push("Hello, #{attendees}! You'll be assigned to room #{room}!")
 
   end
   room_assignments
